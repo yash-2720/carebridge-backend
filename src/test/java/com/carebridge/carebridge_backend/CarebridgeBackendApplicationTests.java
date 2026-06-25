ec33af6a4d1cd0a9615c8394d9785f76257823c3
yash-2720 <mnyashwanthkumar27@gmail.com>
2026-06-25 21:00:40 +0530

    initialize Spring Boot project

---
+++ src/test/java/com/carebridge/carebridge_backend/CarebridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carebridge.carebridge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarebridgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
