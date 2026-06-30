ccf328192508a98dc99c14a4b8a07026a7ab2ee9
yash-2720 <mnyashwanthkumar27@gmail.com>
2026-06-30 12:28:13 +0530

    Id Sequence Generator and Employee mapper added

---
+++ src/main/java/com/carebridge/carebridge_backend/dto/response/EmployeeResponseDTO.java
@@ -0,0 +1,17 @@
+package com.carebridge.carebridge_backend.dto.response;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class EmployeeResponseDTO {
+
+    private String employeeId;
+    private String employeeNumber;
+    private String employeeName;
+    private String employeeEmail;
+    private String employeePhoneNumber;
+    private BigDecimal basicSalary;
+    private boolean isActive;
+}
---
+++ src/main/java/com/carebridge/carebridge_backend/sequence/SequenceGenerator.java
@@ -0,0 +1,7 @@
+package com.carebridge.carebridge_backend.sequence;
+
+public interface SequenceGenerator {
+	
+	String generateId(String code);
+
+}
